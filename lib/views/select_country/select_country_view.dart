c9bea3a3a9ff3bf3e6a3ee6f317577e612a4b332
Joel Stephen <joel.stephen56.sj@gmail.com>
2024-08-12 12:56:38 +0100

    feat/implemented the appbar on select country view

--- lib/views/select_country/select_country_view.dart
+++ lib/views/select_country/select_country_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hotel_it/views/select_country/select_country_viewmodel.dart';
 import 'package:stacked/stacked.dart';
 
@@ -14,7 +16,28 @@ class SelectCountryView extends StatelessWidget {
         SelectCountryViewModel model,
         Widget? child,
       ) {
-        return const Scaffold(
+        return Scaffold(
+          appBar: AppBar(
+            leading: Padding(
+              padding: EdgeInsets.only(left: 12.w, top: 15.h),
+              child: GestureDetector(
+                onTap: Navigator.of(context).pop,
+                child: Text(
+                  "Back",
+                  style: GoogleFonts.poppins(),
+                ),
+              ),
+            ),
+            actions: [
+              Text(
+                "Step 2 of 2",
+                style: GoogleFonts.poppins(),
+              ),
+              SizedBox(
+                width: 15.w,
+              )
+            ],
+          ),
           body: Center(
             child: Text(
               'SelectCountryView',
