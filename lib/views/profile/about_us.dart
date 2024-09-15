43e0bef1a481f565b00e7f29f3e55f91d1bda3f8
Blessing Wisdom <88656234+blessingwisdom@users.noreply.github.com>
2024-09-15 23:54:21 +0100

    Made UI changes to AboutUs View

--- lib/views/profile/about_us.dart
+++ lib/views/profile/about_us.dart
@@ -28,7 +28,6 @@ class AboutUsView extends StatelessWidget {
             body: Padding(
                 padding: EdgeInsets.only(left: 20.w, right: 20.w),
                 child: Text(
-                  softWrap: true,
                   ksLoremIpsum,
                   style: w400Style(
                       17, Theme.of(context).colorScheme.onSurfaceVariant),
