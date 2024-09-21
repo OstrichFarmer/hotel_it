9a8276e8bc8336b52bae1e0638097a4eb5d1382c
Blessing Wisdom <88656234+blessingwisdom@users.noreply.github.com>
2024-09-21 23:45:46 +0100

    Added payment option page

---
+++ lib/views/payment/payment_option_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PaymentOptionView extends StatelessWidget {
+  const PaymentOptionView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
