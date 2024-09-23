e2e323a053cf8dbf95043055ed97b9fb0d399ff6
Joel Stephen <joel.stephen56.sj@gmail.com>
2024-09-23 21:05:57 +0100

    implemented the range picker date

--- lib/services/network/constant.dart
+++ lib/services/network/constant.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const Color primaryColor = Color(0xff109A27);
+const Color lightGreen = Color(0XFFD0EBD4);
--- lib/views/schedule_view/schedule_view.dart
+++ lib/views/schedule_view/schedule_view.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hotel_it/services/network/constant.dart';
 import 'package:hotel_it/views/schedule_view/schedule_viewmodel.dart';
 import 'package:stacked/stacked.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 
 class ScheduleView extends StatelessWidget {
   const ScheduleView({super.key});
@@ -14,13 +18,62 @@ class ScheduleView extends StatelessWidget {
         ScheduleViewModel model,
         Widget? child,
       ) {
-        return const Scaffold(
-          body: Center(
-            child: Text(
-              'ScheduleView',
+        return Scaffold(
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            appBar: AppBar(
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              centerTitle: true,
+              title: Text(
+                "Schedule",
+                style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
             ),
-          ),
-        );
+            body: Padding(
+              padding: EdgeInsets.only(
+                left: 15.w,
+                right: 15.w,
+                top: 20.h,
+                bottom: 20.h,
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border:
+                          Border.all(color: const Color.fromRGBO(0, 0, 0, 1)),
+                    ),
+                    child: SfDateRangePicker(
+                      monthCellStyle: DateRangePickerMonthCellStyle(
+                        todayTextStyle: TextStyle(
+                            color:
+                                Theme.of(context).colorScheme.onSecondaryFixed),
+                        textStyle: TextStyle(
+                            color: Theme.of(context).colorScheme.onSurface),
+                      ),
+                      headerStyle: DateRangePickerHeaderStyle(
+                          backgroundColor:
+                              Theme.of(context).colorScheme.primary),
+                      selectionTextStyle: TextStyle(
+                          color: Theme.of(context).colorScheme.onSurface),
+                      startRangeSelectionColor:
+                          Theme.of(context).colorScheme.onSecondaryFixed,
+                      endRangeSelectionColor:
+                          Theme.of(context).colorScheme.onSecondaryFixed,
+                      todayHighlightColor:
+                          Theme.of(context).colorScheme.onSecondaryFixed,
+                      selectionColor:
+                          Theme.of(context).colorScheme.onSecondaryFixed,
+                      rangeSelectionColor: lightGreen,
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      view: DateRangePickerView.month,
+                      selectionMode: DateRangePickerSelectionMode.range,
+                    ),
+                  ),
+                ],
+              ),
+            ));
       },
     );
   }
